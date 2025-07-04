f4871dac9aab7c01a056e5d4502c2ed7c0b22805
Quinterock <luisgqr1975@gmail.com>
2025-07-04 14:12:20 -0600

    Login v2

--- DesignPatterns/Components/Cells/HeroCell.swift
+++ DesignPatterns/Components/Cells/HeroCell.swift
@@ -11,4 +11,27 @@ final class HeroCell: UITableViewCell {
         nameLabel.text = name
         avatarView.setImage(photo)
     }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupCardStyle()
+    }
+    
+    private func setupCardStyle() {
+        contentView.backgroundColor = .clear // So the shadow can be visible
+        
+        // Create a view container to apply shadow and corner radius
+        let backgroundCardView = UIView(frame: contentView.bounds)
+        backgroundCardView.backgroundColor = .white
+        backgroundCardView.layer.cornerRadius = 12
+        backgroundCardView.layer.shadowColor = UIColor.black.cgColor
+        backgroundCardView.layer.shadowOpacity = 0.15
+        backgroundCardView.layer.shadowOffset = CGSize(width: 0, height: 4)
+        backgroundCardView.layer.shadowRadius = 8
+        backgroundCardView.layer.masksToBounds = false
+        
+        backgroundCardView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        contentView.insertSubview(backgroundCardView, at: 0)
+        
+    }
 }
